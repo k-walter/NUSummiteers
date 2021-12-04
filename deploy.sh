b0e36c593263f7e1494480a21c187d62010ff2bb
Walter Kong <walter.98.kong@gmail.com>
2021-12-05 02:30:44 +0800

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 read -p "Heroku app name: " APP_NAME
+#APP_NAME=nusummiteers
 heroku container:login
 heroku plugins:install heroku-config
-heroku config:push --app $APP_NAME
+heroku config:push -o --app $APP_NAME # -o overwites upstream -c deletes from upstream
 heroku container:push bot --app $APP_NAME
 heroku container:release bot --app $APP_NAME
 echo "Successfully deployed!"
-
